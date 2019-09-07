bf5b63805d17df3100b1da9ad8e32021e9575ae9
Vincent Teng <vincentt117@gmail.com>
2019-09-07 10:19:28 -0400

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+import '../node_modules/react-vis/dist/style.css';
+import {XYPlot, LineSeries} from 'react-vis';
+import TextBox from './components/textbox';
+
+class App extends Component {
+  render() {
+    const data = [
+      {x: 0, y: 8},
+      {x: 1, y: 5},
+      {x: 2, y: 4},
+      {x: 3, y: 9},
+      {x: 4, y: 1},
+      {x: 5, y: 7},
+      {x: 6, y: 6},
+      {x: 7, y: 3},
+      {x: 8, y: 2},
+      {x: 9, y: 0}
+    ];
+    return (
+      <div className="App">
+        <TextBox></TextBox>
+        <XYPlot height={300} width={1000}>
+          <LineSeries data={data} />
+        </XYPlot>
+      </div>
+    );
+  }
+}
+
+export default App;
